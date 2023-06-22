2965ab52b1181465067627ca02df341e6f43b0b8
Dharvik19 <dharvik.badga22@gmail.com>
2023-06-22 20:44:39 +0530

    understanding prerendering

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,88 @@
-import MeetupDetail from "../../components/meetups/MeetupDetail";
-function MeetUpDetails() {
-  <MeetupDetail
-  image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg'
-  title='First Meetup'
-  address='Some Street 5, Some City'
-  description='This is a first meetup'
-  />;
+import { useRouter } from 'next/router'
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This is a first meetup!'
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 10, 12345 Some City',
+    description: 'This is a second meetup!'
+  },
+  {
+    id: 'm3',
+    title: 'A third Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Some address 12, 12345 Some City',
+    description: 'This is a third meetup!'
+  }
+];
+function MeetupDetails() {
+ const router = useRouter()
+ const id = router.query.meetupId;
+ console.log(id)
+  return (
+    <>
+    {DUMMY_MEETUPS.filter((item)=>item.id ===id ).map((item)=>(
+      <MeetupDetail
+      image={item.image}
+      title={item.title}
+      address={item.address}
+      description={item.description}
+    />
+    ))}
+    </>
+  );
 }
-export default MeetUpDetails;
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm3',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
